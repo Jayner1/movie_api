67433a740efbec4bb7a3336ee9b1be9b7c39f2ff
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-11-16 09:25:25 -0800

    test

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.get('/',(req,res) => {
   res.send('Welcome to MyFlix!');
 });
 
-app.get('/movies', passport.authenticate('jwt', { session: false }),(req, res) => {
+app.get('/movies', (req, res) => {
 		Movies.find()
 			.then((movies) => {
 				res.status(200).json(movies);
@@ -107,6 +107,8 @@ app.get('/movies', passport.authenticate('jwt', { session: false }),(req, res) =
 				res.status(500).send('Error: ' + err);
 			});
 	});
+
+  // passport.authenticate('jwt', { session: false })
 
 //read
 app.get('/users', passport.authenticate('jwt', { session: false }),(req, res) => {
